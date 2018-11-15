57b70a86c0289652c5234187b871eca250e8573a
Feichtinger <michael.feichtinger@DEV42.local>
2018-11-15 17:31:00 +0100

    ClientId added

--- example/index.php
+++ example/index.php
@@ -3,24 +3,32 @@
     Class Flag {
 
         public static function clientId() {
-            if (isset($_COOKIE['flg'])) {
-                return $_COOKIE['flg'];
-            } else {
-                $clientId = md5($_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR']);
-                setcookie("flg", $clientId);
-                return $clientId;
+            if (isset($_COOKIE['flg-id'])) {
+                return $_COOKIE['flg-id'];
             }
+            return '';
         }
 
         protected static function request($flagName, $clientId) {
+
             $handle = curl_init();
             $url = "http://localhost:8080/flag/$flagName/$clientId";
             curl_setopt($handle, CURLOPT_URL, $url);
+            curl_setopt($handle, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+            curl_setopt($handle, CURLOPT_HTTPHEADER, [
+                'Accept-Language: '.$_SERVER['HTTP_ACCEPT_LANGUAGE'],
+                'Remote-Address: '.$_SERVER['REMOTE_ADDR']
+            ]);
             curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
             $result = curl_exec($handle);
-            // var_dump($url, $result);
             curl_close($handle);
-            return json_decode($result, true);
+
+            $json = json_decode($result, true);
+            if (isset($json['clientId'])) {
+                setcookie("flg-id", $json['clientId']);
+            }
+
+            return $json;
         }
 
         public static function isActive($flagName) {
@@ -40,13 +48,22 @@
 <head>
     <meta charset="UTF-8">
     <title>FLGR Example File</title>
+    <style>
+        header, main, aside { box-sizing:border-box; font-family:Helvetica; font-size:48px; color:#fff; font-weight:bold; text-align:center; }
+        header { height:100px; background:#FF4C3B; margin:1%; }
+        main { float:left; height:400px; width:78%; background:#0072BB; margin:1%; }
+        aside { float:right; height:400px; width:18%; background:#FF4C3B; margin:1%; }
+    </style>
 </head>
 <body>
-    <?php if(Flag::isActive('feature-one')): ?>
-        <section>Feature One is active!</section>
-    <?php else: ?>
-        <section>No Feature One!</section>
+    <?php if(Flag::isActive('my-hero')): ?>
+        <header>HERO</header>
     <?php endif; ?>
 
+    <main>MAIN CONTENT</main>
+
+    <?php if(Flag::isActive('my-sidebar')): ?>
+        <aside>SIDEBAR</aside>
+    <?php endif; ?>
 </body>
 </html>
--- noname.go
+++ noname.go
@@ -6,6 +6,9 @@ import (
   "net/http"
   "strings"
   "log"
+  "crypto/md5"
+  "encoding/hex"
+  "io"
 )
 
 // create a flag
@@ -14,17 +17,39 @@ import (
 
 
 type Flag struct {
-  Name   string
-  Status bool
+  Name     string
+  Status   bool
+  ClientId string
 }
+
+func createClientId(r *http.Request) string {
+    remoteAddr := r.RemoteAddr
+    userAgent := r.Header.Get("User-Agent")
+    acceptLanguage := r.Header.Get("Accept-Language")
+    // remoteAddr := r.Header.Get("X-FORWARDED-FOR")
+
+    h := md5.New()
+    io.WriteString(h, remoteAddr)
+    io.WriteString(h, userAgent)
+    io.WriteString(h, acceptLanguage)
+
+    return hex.EncodeToString(h.Sum(nil))
+}
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	path := strings.Split(r.URL.Path, "/")
 	flagName := string(path[2])
 
+    clientId := string(path[3])
+    if clientId == "" {
+        clientId = createClientId(r)
+        // fmt.Println(clientId)
+    }
+
 	// debug
-	flag := Flag{flagName, false}
+	flag := Flag{flagName, false, clientId}
 
 	js, err := json.Marshal(flag)
 	if err != nil {
@@ -34,7 +59,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 
-	fmt.Println(string(js))
+	fmt.Println(clientId)
 }
 
 func main() {
